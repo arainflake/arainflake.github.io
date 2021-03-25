38ece26229068d941b8012648f05b62f1b118214
arainflake <74027698+arainflake@users.noreply.github.com>
2021-03-25 04:23:00 +0000

    Update night-day-mode.js

--- night-day-mode.js
+++ night-day-mode.js
@@ -4,17 +4,17 @@ function nightDayMode()
 	var y = document.getElementById("day-css");
 	var z = document.getElementById("night-css");
 	
-	if (x.src.match("sun.png") == "sun.png")
+	if (x.src.match("sun.svg") == "sun.svg")
 	{
 		//night mode
-		x.src = "./moon.png";
+		x.src = "./moon.svg";
 		y.disabled = false;
 		z.disabled = true;
 	}
 	else
 	{
 		//day mode
-		x.src = "./sun.png";
+		x.src = "./sun.svg";
 		y.disabled = true;
 		z.disabled = false; 
 	}
